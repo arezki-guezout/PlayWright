22751b38db035ce931257ce60b163a8b08a3042c
guezout arezki <arezki.guezout@gmail.com>
2025-03-12 13:03:04 +0100

    Ajout d une tache before each

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/login.page';
+test.describe("scenario de connexion", () => {
+let lp:LoginPage;
 
-test('login to saucedemo', async ({ page })=>{
+test.beforeEach("connexion a sauce demo", async ({page})=>{
   await page.goto("https://www.saucedemo.com/");
   await expect(page).toHaveTitle(/Swag Labs/);
-  const lp:LoginPage = new LoginPage(page)
+  lp = new LoginPage(page)
+})
+
+test('login to saucedemo', async ({ page })=>{
   await lp.saisirUserName("standard_user")
   await lp.saisirPassword("secret_sauce")
   await lp.cliquerBtn()
@@ -12,11 +17,9 @@ test('login to saucedemo', async ({ page })=>{
 })
 
 test('login to saucedemo with error', async ({ page })=>{
-  await page.goto("https://www.saucedemo.com/");
-  await expect(page).toHaveTitle(/Swag Labs/);
-  const lp:LoginPage=new LoginPage(page)
   await lp.saisirUserName("standard_user_incorrect")
   await lp.saisirPassword("secret_sauce")
   await lp.cliquerBtn()
   await expect(await lp.getErrorMsg()).toBeVisible()
+})
 })
--- tests/produit.spec.ts
+++ tests/produit.spec.ts
@@ -2,17 +2,24 @@ import { expect, test } from "@playwright/test";
 import ProductPage from "../pages/produit.page";
 import LoginPage from "../pages/login.page";
 
-test('ajouter un produit', async({ page }) => {
+test.describe("Ajout et suppression de produit", ()=>{
+    let lp:LoginPage
+    let pp:ProductPage
+    
+    test.beforeEach("Connexion a sauce demo", async ({page})=>{
         await page.goto("https://www.saucedemo.com/");
         await expect(page).toHaveTitle(/Swag Labs/);
-        const lp:LoginPage = new LoginPage(page)
+        lp = new LoginPage(page)
         await lp.saisirUserName("standard_user")
         await lp.saisirPassword("secret_sauce")
         await lp.cliquerBtn()
-        const pp: ProductPage = new ProductPage(page);
+    })
+
+    test('ajouter un produit', async({ page }) => {
+        pp= new ProductPage(page);
         await pp.ajouterProduit();
         await expect(await pp.getBadge()).toBeVisible();
         await pp.supprimerProduit();
         await expect(await pp.getBadge()).not.toBeVisible();
-
     })
+})
