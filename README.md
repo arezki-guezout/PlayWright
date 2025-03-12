e594d0994f2478b09c8723453d496f96d3bb9806
guezout arezki <arezki.guezout@gmail.com>
2025-03-12 10:57:30 +0100

    kkkk

---
+++ README.md
@@ -0,0 +1,8 @@
+1- Installation de NodeJS
+    sudo apt install nodejs
+2- Installation de Playwright
+    - npm init playwright@latest
+    - laiser les valeurs par défaut pour toutes les questions posées (tapez Entrer)
+3- Création d'un premier test
+    - les tests doivent être écrit dans le répertoire tests
+    - lancer les tests avec la commande npx playwright test
---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('login to saucedemo', async ({ page })=>{
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle(/Swag Labs/);
+  await page.locator("#user-name").fill("standard_user")
+  await page.locator("#password").fill("secret_sauce")
+  await page.locator("#login-button").click()
+  await expect(page).toHaveURL(/inventory/)
+})
+
+test('login to saucedemo', async ({ page })=>{
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page).toHaveTitle(/Swag Labs/);
+  await page.locator("#user-name").fill("standard_user_incorrect")
+  await page.locator("#password").fill("secret_sauce")
+  await page.locator("#login-button").click()
+  await expect(page.locator("h3[data-test=error]")).toBeVisible()
+})
